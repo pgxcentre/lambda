ab653203cbed7cea0d69aa0ee032293b6208eeb4
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2016-01-06 10:57:04 -0500

    First commit

---
+++ compute_lambda.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+
+import os
+import sys
+import logging
+import argparse
+
+import pandas as pd
+
+
+__copyright__ = "Copyright 2014, Beaulieu-Saucier Pharmacogenomics Centre"
+__license__ = "MIT"
+__version__ = "0.1"
+
+
+# Logging configuration
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s %(name)s %(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger("lambda")
+
+
+def main():
+    """The main function."""
+    args = parse_args()
+    check_args(args)
+
+    for fn in args.i_filenames:
+        logger.info("Reading " + fn)
+        data = pd.read_csv(fn, sep=args.delim, usecols=[args.field])
+        data = data.dropna()
+
+        stats = data[args.field]
+        if not args.chi:
+            stats = stats ** 2
+        l = max(stats.median() / 0.456, 1)
+        logger.info("  lambda = {:.6f}".format(round(l, 6)))
+
+
+def check_args(args):
+    """Check arguments and options.
+
+    Args:
+        args (argparse.Namespace): the arguments and options.
+
+    """
+    final_filenames = []
+    for fn in args.i_filenames:
+        # Checking the file exists
+        if not os.path.isfile(fn):
+            logger.critical("{}: no such file".format(fn))
+            sys.exit(1)
+
+        # Checking the column exists for each file
+        with open(fn, "r") as i_file:
+            header = set(i_file.readline().rstrip("\r\n").split(args.delim))
+            if args.field not in header:
+                logger.critical("{}: no field named {}".format(fn, args.field))
+                sys.exit(1)
+
+
+def parse_args():
+    """Argument parser."""
+    # Creating the parser
+    parser = argparse.ArgumentParser(
+        description="Compute inflation factor (lambda) in GWAS results.",
+    )
+
+    # The version of the script
+    parser.add_argument("-v", "--version", action="version",
+                        version="lambda-" + __version__)
+
+    # Add the INPUT group
+    group = parser.add_argument_group("INPUT FILES")
+    group.add_argument("-i", "--input", required=True, nargs="+",
+                       metavar="FILE", dest="i_filenames",
+                       help="The list of files containing GWAS results.")
+
+    group.add_argument("-d", "--delim", default="\t", metavar="DELIM",
+                       help="The field delimiter (default is a tabulation).")
+
+    group.add_argument("-f", "--field", required=True, metavar="NAME",
+                       help="The name of the field containing the statistics.")
+
+    group.add_argument("--chi", action="store_true",
+                       help="Statistics were computed using a chi-squared "
+                            "distribution.")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
